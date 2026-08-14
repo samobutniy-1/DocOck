fc07d7969851848afb5ad56617b3860313247af6
webOwl <nikitaterentev916@gmail.com>
2026-08-14 12:19:00 +0200

    feat: add header component and base project styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './core/layout/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('dockock');
-}
+export class App {}
---
+++ src/app/core/layout/header.ts
@@ -0,0 +1,9 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {}
