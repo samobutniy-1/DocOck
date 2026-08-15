23036f752a88cb223acd22188f3863b7544c7b10
webOwl <nikitaterentev916@gmail.com>
2026-08-15 16:40:22 +0200

    refactor: remove aside, minor cleanup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './core/layout/header';
-import { Aside } from './features/home/aside/aside';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Aside],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
