9da1e9846876c71120354fe770099525208d53b5
webOwl <nikitaterentev916@gmail.com>
2026-08-15 14:30:05 +0200

    feat: add aside component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './core/layout/header';
+import { Aside } from './features/home/aside/aside';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Aside],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
